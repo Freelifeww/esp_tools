aaeb41e1c00b6b3c231c7ba6a79f66c994450983
526986461@qq.com <526986461@qq.com>
2022-03-26 02:00:58 +0800

    data structure modification

--- src/main.py
+++ src/main.py
@@ -1,6 +1,12 @@
-# author:wangwei
-# time: 2022/3/24
-from __future__ import division, print_function
+#!/usr/bin/env python
+#
+# ESP8266 & ESP32 ROM Bootloader Utility
+# Copyright (C) 2014-2016 Fredrik Ahlberg, Angus Gratton, Espressif Systems (Shanghai) PTE LTD, other contributors as noted.
+# https://github.com/Freelifeww/esp_tools
+#
+# This program is based on the burning script of Lexin continue to further interface 
+# related efuse burning and file burning operation, to achieve a key operation to complete 
+# the burning you want, do not repeatedly input different commands
 
 import argparse
 import io
@@ -10,34 +16,42 @@ import struct
 import sys
 import time
 
-#efuse status write
-espefuse_tools = 'espefuse.py -p'
-espefuse_cmds_summary = 'summary'
-espefuse_cmds_burn_efuse = 'burn_efuse'
-espefuse_cmds_burn_key = 'burn_key'
-espefuse_cmds_burn_key_file = 'flash_encryption'
-espefuse_cmds_burn_key_file_name = 'burn_key.BLK1.bin'
-espefuse_cmds_jtag_disable_1 = 'JTAG_DISABLE 1'
-espefuse_cmds_encrypt_disable_1 = 'DISABLE_DL_ENCRYPT 1'
-espefuse_cmds_decrypt_disable_1 = 'DISABLE_DL_DECRYPT 1'
-espefuse_cmds_cache_disable_1 = 'DISABLE_DL_CACHE 1'
-espefuse_cmds_flash_crypt_cnt_1 = 'FLASH_CRYPT_CNT 1'
-espefuse_cmds_flash_crypt_config_15 = 'FLASH_CRYPT_CONFIG 15'
-espefuse_cmds_write_protect_efuse = 'write_protect_efuse'
-espefuse_cmds_flash_cnt = 'FLASH_CRYPT_CNT'
 
-#file write
+espefuse_cmd = [
+    ('espefuse.py -p %s summary'),
+    ('espefuse.py -p %s burn_efuse DISABLE_DL_CACHE 1'),
+    ('espefuse.py -p %s burn_efuse JTAG_DISABLE 1'),
+    ('espefuse.py -p %s burn_efuse DISABLE_DL_ENCRYPT 1'),
+    ('espefuse.py -p %s burn_efuse DISABLE_DL_DECRYPT 1'),
+    ('espefuse.py -p %s burn_efuse FLASH_CRYPT_CONFIG 15'),
+    ('espefuse.py -p %s burn_efuse FLASH_CRYPT_CNT 1'),
+    ('espefuse.py -p %s write_protect_efuse FLASH_CRYPT_CNT'),
+    ('espefuse.py -p %s burn_key flash_encryption burn_key.BLK1.bin'),
+    ('espefuse.py -p %s summary'),
+]
 
-def input_serial_com():
-    print("Please enter the device connection serial port name such as COM1")
-    print("Type 'COM1' (all capitals) to continue.")
+esptool_cmd = [
+    ('esptool.py -p %s -b 460800 --before default_reset --after no_reset write_flash --flash_mode dio --flash_size detect --flash_freq 40m 0x1000 flash.4096.bin 0xF000 flash.61440.bin 0x15000 flash.86016.bin 0x20000 flash.131072.bin 0x3d8000 flash.4030464.bin')
+]
+
+input_prinf_cmd = [
+    ('serial','Please enter the device connection serial port name such as COM1 \n Type "COM1" (all capitals) to continue.'),
+    ('sucess','Project burning successful any button to end the burning window'),
+    ('fail','Project burning error, please save log, press any key to end'),
+]
+
+def input_serial_com(input_string):
+    for cmd_string in input_prinf_cmd:
+        #if cmd_string[0] == input_string
+        print(cmd_string[1])
     sys.stdout.flush()  #required for Pythons which disable line buffering, ie mingw in mintty
     try:
-        com_str = raw_input()  # raw_input renamed to input in Python 3
+        user_input_str = raw_input()  # raw_input renamed to input in Python 3
     except NameError:
-        com_str = input()
-    return com_str
-
+        user_input_str = input()
+    return user_input_str
+    
+"""
 def input_sucess_com():
     print("Project burning successful any button to end the burning window")
     sys.stdout.flush()  #required for Pythons which disable line buffering, ie mingw in mintty
@@ -55,81 +69,36 @@ def input_file_com():
     except NameError:
         com_str = input()
     return com_str
-
+"""
 
 def err_exit(ret,str):
     if ret != 0 :
         print("%s err exit" % str)
-        input_file_com()
+        input_serial_com(input_prinf_cmd[0])
         sys.exit(ret)
     print("%s success" % str)
 
 #need write efuse data string
-def efuse_write_all(com_number):
-    esptool_str = ('%s %s %s' % (espefuse_tools,com_number,espefuse_cmds_summary)) 
-    print("%s ....." %esptool_str)
-    ret = os.system(esptool_str)
-    err_exit(ret,esptool_str)
-
-    esptool_str = ('%s %s %s %s' % (espefuse_tools,com_number,espefuse_cmds_burn_efuse,espefuse_cmds_cache_disable_1)) 
-    print("%s ....." %esptool_str)
-    ret = os.system(esptool_str)
-    err_exit(ret,esptool_str)
-
-    esptool_str = ('%s %s %s %s' % (espefuse_tools,com_number,espefuse_cmds_burn_efuse,espefuse_cmds_jtag_disable_1)) 
-    print("%s ....." %esptool_str)
-    ret = os.system(esptool_str)
-    err_exit(ret,esptool_str)
-
-    esptool_str = ('%s %s %s %s' % (espefuse_tools,com_number,espefuse_cmds_burn_efuse,espefuse_cmds_encrypt_disable_1)) 
-    print("%s ....." %esptool_str)
-    ret = os.system(esptool_str)
-    err_exit(ret,esptool_str)
-
-    esptool_str = ('%s %s %s %s' % (espefuse_tools,com_number,espefuse_cmds_burn_efuse,espefuse_cmds_decrypt_disable_1)) 
-    print("%s ....." %esptool_str)
-    ret = os.system(esptool_str)
-    err_exit(ret,esptool_str)
-
-    esptool_str = ('%s %s %s %s' % (espefuse_tools,com_number,espefuse_cmds_burn_efuse,espefuse_cmds_flash_crypt_config_15)) 
-    print("%s ....." %esptool_str)
-    ret = os.system(esptool_str)
-    err_exit(ret,esptool_str)
-
-    esptool_str = ('%s %s %s %s' % (espefuse_tools,com_number,espefuse_cmds_burn_efuse,espefuse_cmds_flash_crypt_cnt_1)) 
-    print("%s ....." %esptool_str)
-    ret = os.system(esptool_str)
-    err_exit(ret,esptool_str)
-
-    esptool_str = ('%s %s %s %s' % (espefuse_tools,com_number,espefuse_cmds_write_protect_efuse,espefuse_cmds_flash_cnt)) 
-    print("%s ....." %esptool_str)
-    ret = os.system(esptool_str)
-    err_exit(ret,esptool_str)
-
-    esptool_str = ('%s %s %s %s %s' % (espefuse_tools,com_number,espefuse_cmds_burn_key,espefuse_cmds_burn_key_file,espefuse_cmds_burn_key_file_name)) 
-    print("%s ....." %esptool_str)
-    ret = os.system(esptool_str)
-    err_exit(ret,esptool_str)
-
-    esptool_str = ('%s %s %s' % (espefuse_tools,com_number,espefuse_cmds_summary)) 
-    print("%s ....." %esptool_str)
-    ret = os.system(esptool_str)
-    err_exit(ret,esptool_str)
-    print("..........eufuse all write sucess..............")
+def efuse_tools_cmd(com_number):
+    for cmd_string in espefuse_cmd:
+        cmd_string = (cmd_string % com_number)
+        print(cmd_string)
+        ret = os.system(cmd_string)
+        err_exit(ret,cmd_string)
 
 #need write file data string
-def esp_file_write_all(com_number):
-    esptool_str = ('esptool.py -p %s -b 460800 --before default_reset --after no_reset write_flash --flash_mode dio --flash_size detect --flash_freq 40m 0x1000 flash.4096.bin 0xF000 flash.61440.bin 0x15000 flash.86016.bin 0x20000 flash.131072.bin 0x3d8000 flash.4030464.bin' % com_number)
-    print("%s ....." %esptool_str)
-    ret = os.system(esptool_str)
-    err_exit(ret,esptool_str)
+def esp_file_tools_cmd(com_number):
+    for cmd_string in esptool_cmd:
+        cmd_string = (cmd_string % com_number)
+        print(cmd_string)
+        ret = os.system(cmd_string)
+        err_exit(ret,cmd_string)
 
 def main(custom_commandline=None):
-    com_number = input_serial_com()
-    esp_file_write_all(com_number)
-    print("..........eufuse file write sucess..............")
-    efuse_write_all(com_number)
-    input_sucess_com()
+    com_number = input_serial_com(input_prinf_cmd[0])
+    #esp_file_tools_cmd(com_number)
+    #efuse_tools_cmd(com_number)
+    #input_sucess_com()
 
 def _main():
     try:
